dadd429d517e0ddc482a9b5c7d33e951c76ccec3
xiaxiangnan <xiaxiangnan@didichuxing.com>
2016-02-19 16:55:25 +0800

    init

--- src/com/journey/multithreading/create/RunnableNewThread.java
+++ src/com/journey/multithreading/create/RunnableNewThread.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.journey.multithreading.create;
 
 /**
@@ -31,38 +30,4 @@ public class RunnableNewThread implements Runnable {
         t1.start();
         t2.start();
     }
-=======
-package com.journey.multithreading.create;
-
-/**
- * 实现Runnable接口的多线程
- * PROJECT: multithreading
- * Created by xiaxiangnan on 15-12-2.
- */
-public class RunnableNewThread implements Runnable {
-
-    private String name;
-
-    public RunnableNewThread(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(name + ": " + i);
-        }
-    }
-
-    public static void main(String[] args) {
-        RunnableNewThread ds1 = new RunnableNewThread("张三");
-        RunnableNewThread ds2 = new RunnableNewThread("李四");
-        //通过Thread类调用
-        Thread t1 = new Thread(ds1);
-        Thread t2 = new Thread(ds2);
-
-        t1.start();
-        t2.start();
-    }
->>>>>>> 89a73c1f7e2b35477d263710223f8b8d57bb2ebe
 }
--- src/com/journey/multithreading/synchronize/Test.java
+++ src/com/journey/multithreading/synchronize/Test.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.journey.multithreading.synchronize;
 
 /**
@@ -51,57 +50,3 @@ public class Test {
     }
 
 }
-=======
-package com.journey.multithreading.synchronize;
-
-/**
- * 测试
- *
- * PROJECT: multithreading
- * Created by xiaxiangnan on 15-12-4.
- */
-public class Test {
-
-
-    public synchronized void search() {
-        System.out.println("search -----" + Thread.currentThread().getName());
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("search end -----" + Thread.currentThread().getName());
-    }
-
-    public synchronized void insert() {
-        System.out.println("insert -----" + Thread.currentThread().getName());
-    }
-
-
-
-    public static void main(String[] args) {
-        final Test t1 = new Test();
-
-
-        /**
-         * 如果一个线程在对象上获得一个锁，就没有任何其他线程可以进入（该对象的）类中的任何一个同步方法。
-         */
-        new Thread(){
-            @Override
-            public void run() {
-                t1.search();
-            }
-
-        }.start();
-        new Thread(){
-            @Override
-            public void run() {
-                t1.insert();
-            }
-        }.start();
-
-
-    }
-
-}
->>>>>>> 89a73c1f7e2b35477d263710223f8b8d57bb2ebe
